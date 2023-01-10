e85f8df0a29ded835351ef2daa64f86b22c6bb91
kinanen <otso.kinanen@gmail.com>
2023-01-10 13:51:58 +0200

    1tehtävän 1 osa valmis

--- tehtavat1/kurssitiedot/src/App.js
+++ tehtavat1/kurssitiedot/src/App.js
@@ -1,5 +1,29 @@
+const Header = () => {
+  return (
+    <h1>
+      Half Stack application development
+    </h1>
+  )
+}
+
+const Content =(props) =>{
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total =(argumentti) => {
+  return(
+    <p>
+      Number of exercises {argumentti.summa}
+    </p>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
+
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -9,17 +33,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total summa={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
